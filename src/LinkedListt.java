ba93c4e249952fd6ec25a259844b640de1c2ea23
Sadat <mukul_iiuc@yahoo.com>
2020-03-10 12:46:14 +0100

    Initialize PiD folder

---
+++ src/LinkedListt.java
@@ -0,0 +1,24 @@
+public class LinkedListt {
+
+    Node head;    // First Node is the head Node
+
+    public void insert(int data)
+    {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        if(head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node n = head;
+            while(n.next!=null)
+            {
+
+            }
+        }
+    }
+    }
---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        LinkedListt list = new LinkedListt();
+        list.insert(18);
+
+
+
+
+    }
+}
